1b9773437ee6df7c808fb005cd1190cc9682ca93
Nat Welch <nat@natwelch.com>
2012-04-10 15:27:28 -0700

    striping out play tracking.

--- Shelved.bowtie/javascript.js
+++ Shelved.bowtie/javascript.js
@@ -1,12 +1,11 @@
-var pS = 1;
-function $(el) {return document.getElementById(el);}
+function $(el) { return document.getElementById(el); }
 
 function artworkUpdate(artURL) {
 	$('#album').attr('src',artUrl);
 }
 
 function artworkUpdate(artURL) {
-	if(artURL == "") {
+	if (artURL == "") {
 		$('album').src = "images/no_art.png";
 		$('overlay').src = "images/blank.png";
 	} else {
@@ -14,22 +13,3 @@ function artworkUpdate(artURL) {
 		$('overlay').src = "images/overlay.png";
 	}
 }   
-
-function playPause() {
-	pS = Player.playState();
-	if (pS != (0||2)) { // if playing
-		$('playcontrols').src ="images/play.png"; // change the coffee cup to "pause"
-		Player.playPause(); // tell itunes to pause
-		return false;
-	}
-	
-	else {	// if stopped
-		$('playcontrols').src ="images/pause.png";   // change coffee cup to "play"
-		Player.playPause(); // tell itunes to play
-		return false;
-	}
-}
-
-function changeTrack() {
-	$("fred-link").ondblclick = function () { Player.nextTrack(); } 	
-}
