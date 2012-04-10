e748f759cc5fe4040ff463fc23b7be9881aaede6
Nat Welch <nat@natwelch.com>
2012-04-10 15:16:17 -0700

    copying over Shelved.

---
+++ Shelved.bowtie/javascript.js
@@ -0,0 +1,37 @@
+var pS = 1;
+function $(el) {return document.getElementById(el);}
+
+function artworkUpdate(artURL) {
+	$('#album').attr('src',artUrl);
+}
+
+function artworkUpdate(artURL)
+{
+	if(artURL == "") {
+		$('album').src = "images/no_art.png";
+		$('overlay').src = "images/blank.png";
+	}
+	else {
+		$('album').src = artURL;
+		$('overlay').src = "images/overlay.png";
+	}
+}   
+
+function playPause() {
+	pS = Player.playState();
+	if(pS != (0||2)) { // if playing
+		$('playcontrols').src ="images/play.png"; // change the coffee cup to "pause"
+		Player.playPause(); // tell itunes to pause
+		return false;
+	}
+	
+	else {	// if stopped
+		$('playcontrols').src ="images/pause.png";   // change coffee cup to "play"
+		Player.playPause(); // tell itunes to play
+		return false;
+	}
+}
+
+function changeTrack() {
+	$("fred-link").ondblclick = function () { Player.nextTrack(); } 	
+}
