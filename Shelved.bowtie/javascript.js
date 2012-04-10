f9267487591a4d0ed03992b817fda3e7c780464b
Nat Welch <nat@natwelch.com>
2012-04-10 16:31:52 -0700

    whitespace.

--- Shelved.bowtie/javascript.js
+++ Shelved.bowtie/javascript.js
@@ -1,13 +1,13 @@
 function $(el) { return document.getElementById(el); }
 
 function artworkUpdate(artURL) {
-	if (artURL == "") {
-		$('album').src = "images/no_art.png";
-		$('overlay').src = "images/blank.png";
-	} else {
-		$('album').src = artURL;
-		$('overlay').src = "images/overlay.png";
-	}
+  if (artURL == "") {
+    $('album').src = "images/no_art.png";
+    $('overlay').src = "images/blank.png";
+  } else {
+    $('album').src = artURL;
+    $('overlay').src = "images/overlay.png";
+  }
 }   
 
 function trackChanged(theTrack) {
